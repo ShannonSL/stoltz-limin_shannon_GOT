e27d5e973963029429f04400b249e7fba4309585
ShannonSL <s_stoltz-limin@fanshaweonline.ca>
2018-03-21 16:55:47 -0400

    updated all GOT things

--- js/main.js
+++ js/main.js
@@ -1,39 +1,34 @@
-(() =>
+(() => {
 console.log('video stuff fired!');
 
-const vidPlayer = document.querySelector('video'),
-playButton = document.querySelectorAll('button')[0],
-pauseButton = document.querySelectorAll('button') [1];
-rewindButton = document.querySelectorAll('button') [2];
+// add to the String prototype to cap the first letter
+// review this first thing next week!
+String.prototype.capIt = function() { return this.replace(this.charAt(), this.charAt().toUpperCase()); };
 
-function volOn()
-{
-  vidPlayer.muter= false;
+//variables at the top
+const sigils = document.querySelectorAll('.sigilContainer'),
+      lightbox = document.querySelector('.lightbox'),
+      closeLightbox = document.querySelector('.close-lightbox'),
+      vidPlayer = document.querySelector('video');
+//methods / functions in the middle
+function loadMovie(){
+  //debugger;
+  // 1.turn on the lightbox
+  lightbox.classList.add('show-lightbox');
+
+// 2. grab the right video based on the class name -> the split yields the name
+  var house = this.className.split(' ')[1].capIt();
+
+  vidPlayer.play();
+
 }
 
-function volOff()
-{
-  vidPlayer.muted = true;
-}
-function rewindVid()
-{
+function closeLBox(){
+  lightbox.classList.remove('show-lightbox');
+  vidPlayer.pause();
   vidPlayer.currentTime = 0;
 }
-
-function playVideo()
-{
-  vidPlayer.play();
-}
-function pauseVideo()
-{
-  vidPlayer.paused();
-}
-
-vidPlayer.addEventListener('mouseover', volOn);
-vidPlayer.addEventListener('mouseout', volOff);
-
-rewindButton.addEventListener('click' rewindVid);
-vidPlayer.addEventListener('click', pauseVideo);
-vidPlayer.addEventListener('click', playVideo);
-
+// events at the bottom
+sigils.forEach(sigil => sigil.addEventListener('click', loadMovie));
+closeLightbox.addEventListener('click', closeLBox);
 })();
